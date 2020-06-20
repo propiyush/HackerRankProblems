85ab501190709fb21710e9d09f8addf5c786e330
piyushjindal <piyushjindal@192.168.0.7>
2020-06-20 11:14:12 +0530

    First Commit

---
+++ HackerRankProblems/src/beginnerProblems/HelloWorld.java
@@ -0,0 +1,11 @@
+package beginnerProblems;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
